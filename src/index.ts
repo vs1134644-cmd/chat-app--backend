332c8f0e1a96d9169742e659da52888032d26f85
Tumhara Naam <tum@example.com>
2026-06-23 10:56:28 +0530

    index code

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import AuthRouter from "./router/auth.router";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/auth", AuthRouter);
 
-  mongoose.connect(process.env.DB as string)
-  .then(() => {
-    // console.log("Database Connected");
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.DB as string);
 
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server Running On Port ${process.env.PORT || 8000}`);
     });
-  })
-  .catch((err) => {
+  } catch (err) {
     console.log("MongoDB Error:", err);
-  });
+  }
+};
+
+startServer();
